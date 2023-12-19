4b4f85abfff5877c8453e62a7f7389f24a9e217c
LakshayN <lakshaynarula8888@gmail.com>
2023-12-19 18:40:43 +0530

    context integrated

---
+++ src/utils/mockData.js
@@ -0,0 +1,750 @@
+//sample response in case api gets changed
+
+const res = [
+  {
+    info: {
+      id: "498382",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "M2K Mall",
+      areaName: "Rohini",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.4,
+      feeDetails: {
+        restaurantId: "498382",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3900,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3900,
+      },
+      parentId: "166",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "20 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-19 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹129",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-m2k-mall-rohini-delhi-498382",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "55473",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Sector 10",
+      areaName: "Rohini",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas", "Fast Food"],
+      avgRating: 3.9,
+      feeDetails: {
+        restaurantId: "55473",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3900,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3900,
+      },
+      parentId: "721",
+      avgRatingString: "3.9",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "25 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-19 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-hut-sector-10-rohini-delhi-55473",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "444780",
+      name: "Theobroma",
+      cloudinaryImageId: "b033728dcb0101ceb19bff0e1e1f6474",
+      locality: "Sector 3",
+      areaName: "Rohini",
+      costForTwo: "₹400 for two",
+      cuisines: ["Bakery", "Desserts"],
+      avgRating: 4.5,
+      feeDetails: {
+        restaurantId: "444780",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4700,
+      },
+      parentId: "1040",
+      avgRatingString: "4.5",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 17,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "17 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-18 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "ABOVE ₹159",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/theobroma-sector-3-rohini-delhi-444780",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "772202",
+      name: "Starbucks Coffee",
+      cloudinaryImageId: "0c958153eeb436db6d515ca36e47e8a4",
+      locality: "Reserve Bank Enclave",
+      areaName: "Paschim Vihar",
+      costForTwo: "₹400 for two",
+      cuisines: [
+        "Beverages",
+        "Cafe",
+        "Snacks",
+        "Desserts",
+        "Bakery",
+        "Ice Cream",
+      ],
+      avgRating: 4.5,
+      feeDetails: {
+        restaurantId: "772202",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5000,
+      },
+      parentId: "195515",
+      avgRatingString: "4.5",
+      totalRatingsString: "50+",
+      sla: {
+        deliveryTime: 26,
+        lastMileTravel: 4.8,
+        serviceability: "SERVICEABLE",
+        slaString: "26 mins",
+        lastMileTravelString: "4.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-18 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/starbucks-coffee-reserve-bank-enclave-paschim-vihar-delhi-772202",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "365223",
+      name: "Samosa Fusion",
+      cloudinaryImageId: "cryv5c0c5mwn8fnmy35y",
+      locality: "SEC 3",
+      areaName: "Rohini",
+      costForTwo: "₹200 for two",
+      cuisines: ["Snacks", "Pizzas", "Burgers"],
+      avgRating: 4.2,
+      veg: true,
+      feeDetails: {
+        restaurantId: "365223",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3900,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3900,
+      },
+      parentId: "176544",
+      avgRatingString: "4.2",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 21,
+        lastMileTravel: 0.8,
+        serviceability: "SERVICEABLE",
+        slaString: "21 mins",
+        lastMileTravelString: "0.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-18 21:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/samosa-fusion-sec-3-rohini-delhi-365223",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "26723",
+      name: "KFC",
+      cloudinaryImageId: "56c9ab92bd79745fd152a30fa2525426",
+      locality: "Sector 3",
+      areaName: "Rohini",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      avgRating: 4.2,
+      feeDetails: {
+        restaurantId: "26723",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4700,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4700,
+      },
+      parentId: "547",
+      avgRatingString: "4.2",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "19 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-18 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        subHeader: "UPTO ₹50",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/kfc-sector-3-rohini-delhi-26723",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "253731",
+      name: "McDonald's",
+      cloudinaryImageId: "6dc3ed2ca21d71acff7c2a51dfe4c720",
+      locality: "Sector 3",
+      areaName: "Rohini",
+      costForTwo: "₹400 for two",
+      cuisines: ["American"],
+      avgRating: 4.4,
+      feeDetails: {
+        restaurantId: "253731",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3900,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3900,
+      },
+      parentId: "630",
+      avgRatingString: "4.4",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "20 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-18 23:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mcdonalds-sector-3-rohini-delhi-253731",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "622616",
+      name: "Domino's Pizza",
+      cloudinaryImageId: "hsb5yt3qhechxockrbut",
+      locality: "3rd Sector",
+      areaName: "Rohini",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+      avgRating: 4.4,
+      feeDetails: {
+        restaurantId: "622616",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3900,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 3900,
+      },
+      parentId: "2456",
+      avgRatingString: "4.4",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 25,
+        serviceability: "SERVICEABLE",
+        slaString: "25 mins",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-19 00:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹150 OFF",
+        subHeader: "ABOVE ₹299",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/dominos-pizza-3rd-sector-rohini-delhi-622616",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "341033",
+      name: "Bakingo",
+      cloudinaryImageId: "0c53fd8f62e3d25ba56d1b4cfe572cef",
+      locality: "Rithala",
+      areaName: "Rohini",
+      costForTwo: "₹300 for two",
+      cuisines: ["Bakery", "Desserts", "Beverages", "Snacks"],
+      avgRating: 4.4,
+      feeDetails: {
+        restaurantId: "341033",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5100,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5100,
+      },
+      parentId: "3818",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "20 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-12-19 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-66699bf0-e8c3-4cbf-b965-5299fb1d1bce",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/bakingo-rithala-rohini-delhi-341033",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
+
+export default res;
